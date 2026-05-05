f69315d31f5e37a69f58cec4123a4dbe61df3e28
Gabriel <gabrielveneruci@gmail.com>
2026-05-05 08:26:24 -0300

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,37 @@
+const CACHE_NAME = 'milkshakito-cache-v8';
+const ASSETS = [
+    './',
+    './index.html',
+    './manifest.json',
+    './assets/images/logoshake.png',
+    './assets/images/logopwa.png',
+    './assets/images/logopwa512.png',
+    './assets/images/shakes.png',
+    './assets/images/whatsapp.png'
+];
+
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME).then((cache) => {
+            console.log('Preparando shake no Cache! 🍦');
+            return cache.addAll(ASSETS);
+        })
+    );
+});
+
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request).then((response) => {
+            return response || fetch(event.request);
+        })
+    );
+});
+
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((keys) => {
+            return Promise.all(keys.filter(key => key !== CACHE_NAME).map(key => caches.delete(key))
+            );
+        })
+    );
+});
